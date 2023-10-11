cbe6e88a60b312869178f26a3f9beba71b006ce1
Srebha9924 <144255609+Srebha9924@users.noreply.github.com>
2023-10-11 18:48:04 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,37 @@
-#1.1 implement a recursive function to calculate the factorial of a given number
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+'''Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality.'''
 
 
-number = int(input("enter a number:"))
-res = fact_rec(number)
-print("the factorial of{}is{} .".format(number, res))
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name,initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+    
+  def deposit(self, amount):
+    if amount > 0:
+     self.__account_balance += amount
+     print("Deposited ₹{}. New Balance: ₹{}". format(amount,self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+      
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+     self.__account_balance -= amount 
+     print("Withdrew ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+     print(" Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
+
+
+
+account = BankAccount(account_number="123456789", account_holder_name="Srebha",initial_balance=5000.0)
+
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
+
